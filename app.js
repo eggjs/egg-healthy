8b36aa26c8a616f19bfe0284dd7458186ac598b3
popomore <sakura9515@gmail.com>
2018-11-09 18:07:50 +0800

    f

--- app.js
+++ app.js
@@ -3,10 +3,22 @@
 module.exports = class Lifecycle {
   constructor(app) {
     this.app = app;
+    this.serverHealthStatus = app.serverHealthStatus;
   }
 
   configDidLoad() {
     this.app.config.coreMiddleware.unshift('eggHealthy');
   }
 
+  async serverDidReady() {
+    this.serverHealthStatus.up();
+  }
+
+  async didReady(err) {
+    if (err) this.serverHealthStatus.down();
+  }
+
+  async beforeClose() {
+    this.serverHealthStatus.stop();
+  }
 };
